3fb6b6b55da0c5918f2302a1f713de9ec97aa9d5
legal-swami <legalswamii@gmail.com>
2025-12-21 13:35:02 +0530

    Update ChatRequest.java

--- backend/src/main/java/com/legalswami/model/ChatRequest.java
+++ backend/src/main/java/com/legalswami/model/ChatRequest.java
@@ -1,7 +1,7 @@
 package com.legalswami.model;
 
 import lombok.Data;
-import javax.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 
 @Data
 public class ChatRequest {
